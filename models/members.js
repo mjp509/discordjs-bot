4748a9af51fb0f1a6aaca0a357409649b3a746b2
mjp509 <mjp509@mail.usask.ca>
2022-03-05 18:23:28 -0600

    feat: enum on class and subclasses

--- models/members.js
+++ models/members.js
@@ -12,14 +12,16 @@ const MemberSchema = new Schema({
 	},
 	class_type: {
 		type: String,
+		enum: ["Warrior", "Martial Artist", "Gunner", "Mage", "Assassin"],
 		required: true,
 	},
 	class: {
 		type: String,
+		enum: ["Berserker", "Paladin", "Gunlancer", "Striker", "Wardancer", "Scrapper", "Soulfist", "Gunslinger", "Artillerist", "Deadeye", "Sharpshooter"],
 		required: true,
 	},
 	gear_score: {
-		type: Number, //test push
+		type: Number,
 		required: true,
 	},
 	guild_status: {
