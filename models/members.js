a821e0808e38d1e29e5ea66213f6cce551a5e571
mjp509 <mjp509@mail.usask.ca>
2022-03-05 19:14:55 -0600

    feat: rework db to handle new user onbaord

--- models/members.js
+++ models/members.js
@@ -13,20 +13,16 @@ const MemberSchema = new Schema({
 	class_type: {
 		type: String,
 		enum: ["Warrior", "Martial Artist", "Gunner", "Mage", "Assassin"],
-		required: true,
 	},
 	class: {
 		type: String,
 		enum: ["Berserker", "Paladin", "Gunlancer", "Striker", "Wardancer", "Scrapper", "Soulfist", "Gunslinger", "Artillerist", "Deadeye", "Sharpshooter"],
-		required: true,
 	},
 	gear_score: {
 		type: Number,
-		required: true,
 	},
 	guild_status: {
 		type: String,
-		required: true,
 	},
 });
 
